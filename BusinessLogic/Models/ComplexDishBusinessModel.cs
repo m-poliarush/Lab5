03bac7352e71cd1a53cb490b2c053c7a66f0192c
Makysm Poliarush <polarysh2222@gmail.com>
2025-05-15 00:20:41 +0300

    Added business models

---
+++ BusinessLogic/Models/ComplexDishBusinessModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MenuManager.DB.Models;
+
+namespace BusinessLogic.Models
+{
+    public class ComplexDishBusinessModel : BaseMenuItemBusinessModel
+    {
+        public List<Dish> DishList { get; set; } = new();
+        public void AddDish(Dish dish)
+        {
+            if (dish != null)
+            {
+                DishList.Add(dish);
+            }
+        }
+        public ComplexDishBusinessModel()
+        {
+
+        }
+        public ComplexDishBusinessModel(List<Dish> list)
+        {
+            DishList = list;
+        }
+    }
+}
---
+++ BusinessLogic/Models/OrderBusinessModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MenuManager.DB.Models;
+
+namespace BusinessLogic.Models
+{
+    public class OrderBusinessModel
+    {
+        public int OrderID { get; set; }
+
+        public int TotalCost { get; private set; }
+
+        private ObservableCollection<BaseMenuItem> _dishes = new();
+        public ObservableCollection<BaseMenuItem> dishes
+        {
+            get => _dishes;
+        }
+
+
+        public void AddDish(BaseMenuItem dish)
+        {
+            _dishes.Add(dish);
+            UpdateTotalCost();
+        }
+        public void RemoveDish(BaseMenuItem dish)
+        {
+            _dishes.Remove(dish);
+            UpdateTotalCost();
+        }
+        public void ClearOrderList()
+        {
+            _dishes.Clear();
+            UpdateTotalCost();
+        }
+
+        // Оновлення вартості замовлення
+        private void UpdateTotalCost()
+        {
+            TotalCost = _dishes.Sum(dish => dish.Price);
+        }
+    }
+}
