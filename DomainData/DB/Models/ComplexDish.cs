a0e347dfa27b990e76c400784fa2f87219b36f2f
Makysm Poliarush <polarysh2222@gmail.com>
2025-05-15 11:37:51 +0300

    Deleted business logics from eninties

--- DomainData/DB/Models/ComplexDish.cs
+++ DomainData/DB/Models/ComplexDish.cs
@@ -8,21 +8,7 @@ namespace MenuManager.DB.Models
 {
     public class ComplexDish : BaseMenuItem
     {
-        public List<Dish> DishList { get; set; } = new();
-        public void AddDish(Dish dish)
-        {
-            if(dish != null)
-            {
-                DishList.Add(dish);
-            }
-        }
-        public ComplexDish()
-        {
-
-        }
-        public ComplexDish(List<Dish> list)
-        {
-            DishList = list;
-        }
+        public List<Dish> DishList { get; set; }
+        
     }
 }
--- DomainData/DB/Models/Order.cs
+++ DomainData/DB/Models/Order.cs
@@ -19,27 +19,5 @@ namespace MenuManager.DB.Models
             get => _dishes;
         }
 
-        // Додавання страви через метод
-        public void AddDish(BaseMenuItem dish)
-        {
-            _dishes.Add(dish);
-            UpdateTotalCost();
-        }
-        public void RemoveDish(BaseMenuItem dish)
-        {
-            _dishes.Remove(dish);
-            UpdateTotalCost();
-        }
-        public void ClearOrderList()
-        {
-            _dishes.Clear();
-            UpdateTotalCost();
-        }
-
-        // Оновлення вартості замовлення
-        private void UpdateTotalCost()
-        {
-            TotalCost = _dishes.Sum(dish => dish.Price);
-        }
     }
 }
