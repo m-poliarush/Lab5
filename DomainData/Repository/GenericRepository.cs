70dba9428bb2a2b659a63884d14bedbaad974335
Makysm Poliarush <polarysh2222@gmail.com>
2025-05-17 21:48:48 +0300

    Modified Update method

--- DomainData/Repository/GenericRepository.cs
+++ DomainData/Repository/GenericRepository.cs
@@ -5,17 +5,22 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using MenuManager.DB;
+using MenuManager.DB.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace DomainData.Repository
 {
     public class GenericRepository<TModel> : IGenericRepository<TModel> where TModel : class
     {
         protected readonly DbSet<TModel> _dbSet;
+        protected readonly MenuContext _context;
 
         public GenericRepository(MenuContext context)
         {
             _dbSet = context.Set<TModel>();
+            _context = context;
         }
 
         public TModel GetById(int id, params Expression<Func<TModel, object>>[] includes)
@@ -25,7 +30,8 @@ namespace DomainData.Repository
             foreach (var include in includes)
                 query = query.Include(include);
 
-            return query.FirstOrDefault(e => EF.Property<int>(e, "ID") == id);
+            var keyPropertyName = GetKeyPropertyName(null);
+            return query.FirstOrDefault(e => EF.Property<int>(e, keyPropertyName) == id);
         }
         public List<TModel> GetAll(params Expression<Func<TModel, object>>[] includes)
         {
@@ -37,6 +43,7 @@ namespace DomainData.Repository
             return query.ToList();
         }
 
+
         public void Create(TModel model)
         {
             _dbSet.Add(model);
@@ -44,6 +51,18 @@ namespace DomainData.Repository
 
         public void Update(TModel model)
         {
+            var entry = _context.Entry(model);
+
+            if (entry.State == EntityState.Detached)
+            {
+                var existing = _dbSet.Find(GetIdValue(model));
+                if (existing != null)
+                {
+                    _context.Entry(existing).CurrentValues.SetValues(model);
+                    return;
+                }
+            }
+
             _dbSet.Update(model);
         }
 
@@ -52,6 +71,58 @@ namespace DomainData.Repository
             var entity = _dbSet.Find(id);
             if (entity != null)
                 _dbSet.Remove(entity);
+        }
+        public void Attach(TModel model)
+        {
+            _dbSet.Attach(model);
+        }
+        public TModel GetTrackedOrAttach(int id, params Expression<Func<TModel, object>>[] includes)
+        {
+
+            // Отримуємо ім’я ключової властивості
+            var keyPropertyName = GetKeyPropertyName(null); // null можна, бо метод не використовує сам об'єкт
+            var keyProperty = typeof(TModel).GetProperty(keyPropertyName);
+
+            // Шукаємо відстежувану сутність з таким значенням ключа
+            var trackedEntity = _context.ChangeTracker
+                .Entries<TModel>()
+                .FirstOrDefault(e =>
+                    keyProperty != null &&
+                    keyProperty.PropertyType == typeof(int) &&
+                    (int)keyProperty.GetValue(e.Entity) == id);
+
+            if (trackedEntity != null)
+            {
+                return trackedEntity.Entity;
+            }
+
+            // Шукаємо сутність у базі даних
+            var entity = GetById(id, includes);
+
+            if (entity != null)
+            {
+                _dbSet.Attach(entity);
+            }
+
+            return entity;
+        }
+
+        private int GetIdValue(TModel model)
+        {
+            var propertyName = GetKeyPropertyName(model);
+            var property = typeof(TModel).GetProperty(propertyName);
+            if (property != null && property.PropertyType == typeof(int))
+            {
+                return (int)property.GetValue(model);
+            }
+            throw new InvalidOperationException("Entity does not have an integer ID property");
+        }
+        private string GetKeyPropertyName(TModel model)
+        {
+            var eType = _context.Model.FindEntityType(typeof(TModel));
+            var primaryKey = eType.FindPrimaryKey();
+            var propertyName = primaryKey.Properties[0].Name;
+            return propertyName;
         }
 
     }
--- DomainData/Repository/IGenericRepository.cs
+++ DomainData/Repository/IGenericRepository.cs
@@ -11,11 +11,13 @@ namespace DomainData.Repository
     {
         public TModel GetById(int id, params Expression<Func<TModel, object>>[] includes);
         public List<TModel> GetAll(params Expression<Func<TModel, object>>[] includes);
+        public void Attach(TModel model);
 
         public void Create(TModel model);
 
         public void Update(TModel model);
 
         public void Delete(int id);
+        public TModel GetTrackedOrAttach(int id, params Expression<Func<TModel, object>>[] includes);
     }
 }
